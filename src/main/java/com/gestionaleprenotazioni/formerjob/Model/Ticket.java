258b100c26b9412b76f95423585189758a6c92e3
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-03-30 14:56:54 +0200

    Modified: AbstractMapper.java, Mapper.java, Ticket.java, TicketDto.java, TicketMapper.java

--- src/main/java/com/gestionaleprenotazioni/formerjob/Model/Ticket.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Model/Ticket.java
@@ -1,6 +1,49 @@
 package com.gestionaleprenotazioni.formerjob.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ticket", schema = "formerjob")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ticketId;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event")
+    private Ticket ticket;
 }
