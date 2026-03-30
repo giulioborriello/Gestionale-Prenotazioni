704d5e432ae89f59af47881c2bef54aa6d198640
giuseppe <giuseppe.sodano.1996@gmail.com>
2026-03-30 16:46:39 +0200

    Update: UserController.java UserMapper.java UserService.java

--- src/main/java/com/gestionaleprenotazioni/formerjob/Service/UserService.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Service/UserService.java
@@ -1,44 +1,55 @@
 package com.gestionaleprenotazioni.formerjob.Service;
 
 import com.gestionaleprenotazioni.formerjob.Dto.UserDto;
+import com.gestionaleprenotazioni.formerjob.Mapper.Mapper;
 import com.gestionaleprenotazioni.formerjob.Mapper.UserMapper;
 import com.gestionaleprenotazioni.formerjob.Model.User;
 import com.gestionaleprenotazioni.formerjob.Repository.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService extends AbstractService<UserDto, User>
+public class UserService extends AbstractService<User,UserDto>
 {
-      UserRepository userRepository;
-      UserMapper userMapper;
+    private final UserMapper userMapper;
 
+    private final UserRepository userRepository;
+
+    public UserService(JpaRepository<User, Integer> repository, Mapper<UserDto,User> converter, UserMapper userMapper, UserRepository userRepository) {
+        super(repository, converter);
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
 
     public UserDto findByName(String name)
     {
-        return userMapper.toDTO(userRepository.findByName(name));
+        UserDto userDto = userMapper.toDTO(userRepository.findByName(name));
+        return userDto;
     }
 
     public UserDto findBySurname(String surname)
     {
-        return userMapper.toDTO(userRepository.findBySurname(surname));
+        UserDto userDto = userMapper.toDTO(userRepository.findBySurname(surname));
+        return userDto;
     }
 
     public UserDto findByEmail(String email)
     {
-        return userMapper.toDTO(userRepository.findByEmail(email));
+        UserDto userDto = userMapper.toDTO(userRepository.findByEmail(email));
+        return userDto;
     }
 
     public UserDto findByTaxCode(String taxCode)
     {
-        return userMapper.toDTO(userRepository.findByTaxCode(taxCode));
+        UserDto userDto = userMapper.toDTO(userRepository.findByTaxCode(taxCode));
+        return userDto;
     }
 
-    public UserDto findByNameAndSurname(String name,String surname)
+    public UserDto findByNameAndSurname(String name, String surname)
     {
-        return userMapper.toDTO(userRepository.findByNameAndSurname(name, surname));
+        UserDto UserDto = userMapper.toDTO(userRepository.findByNameAndSurname(name, surname));
+        return UserDto;
     }
-
-
 
 
 }
