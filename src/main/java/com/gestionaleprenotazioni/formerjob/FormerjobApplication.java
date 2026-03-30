d59a9daffb12e8ab54ffb566272080e1992ae27f
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-03-30 12:48:41 +0200

    Add files via upload

---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/FormerjobApplication.java
@@ -0,0 +1,13 @@
+package com.gestionaleprenotazioni.formerjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormerjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormerjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionaleprenotazioni/formerjob/FormerjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionaleprenotazioni.formerjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormerjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
