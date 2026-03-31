07e1d2194054704ceb1f94b84f834fdfc53369c8
Cap1209 <capozzolilorenzo@outlook.it>
2026-03-31 14:16:19 +0200

    Changes Cart.java CartController.java CartDto.java CartMapper.java CartService.java Payment.java PaymentController.java PaymentDto.java PaymentMapper.java PaymentService.java

--- src/main/java/com/gestionaleprenotazioni/formerjob/Model/Payment.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Model/Payment.java
@@ -1,12 +1,12 @@
 package com.gestionaleprenotazioni.formerjob.Model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -17,23 +17,19 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    // Salviamo il nome della costante Enum (es. "CREDIT_CARD") invece dell'indice numerico
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PaymentMethod method;
 
-    // Flag per verificare se il pagamento è andato a buon fine
+
     @Column(nullable = false)
     private Boolean checked;
 
-    // Timestamp della transazione
+
     @Column(nullable = false)
     private LocalDateTime date;
 
-    // Relazione N:1 con Cart.
-    // Più pagamenti possono appartenere allo stesso carrello
-    // (es. più tentativi di pagamento).
-    // La FK cart_id si trova in questa tabella.
     @ManyToOne
     @JoinColumn(name = "cart_id", referencedColumnName = "id", nullable = false)
     private Cart cart;
