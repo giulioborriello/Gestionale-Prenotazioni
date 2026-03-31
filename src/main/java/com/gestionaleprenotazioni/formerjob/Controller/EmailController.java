a30bcafdad3f03013778f97349e2a096e733faae
gotuax <giuseppe.sodano.1996@gmail.com>
2026-03-31 17:03:26 +0200

    Update: application.properties compiler.xml pom.xml Add: EmailController.java EmailService.java

---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Controller/EmailController.java
@@ -0,0 +1,39 @@
+package com.gestionaleprenotazioni.formerjob.Controller;
+
+import com.gestionaleprenotazioni.formerjob.Model.Ticket;
+import com.gestionaleprenotazioni.formerjob.Service.EmailService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/send-test-email")
+    public String sendTestEmail() {
+        String to = "destinatario-di-prova@example.com"; // qualsiasi mail, non verrà usata realmente
+        String subject = "Mail di prova da Spring + Mailtrap";
+        String body = """
+                Ciao da EventIO!
+                Hai Completato l'acquisto del tuo ticket per 
+             
+                """.formatted(LocalDateTime.now());
+
+        try {
+            emailService.sendSimpleEmail(to, subject, body);
+            return "✅ Email di prova inviata (controlla la inbox Mailtrap)";
+        } catch (Exception e) {
+            // Qui puoi anche loggare l'eccezione con un logger
+            return "❌ Errore durante l'invio: " + e.getMessage();
+        }
+    }
+}
