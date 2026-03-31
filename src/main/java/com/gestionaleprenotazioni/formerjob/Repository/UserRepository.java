245c15d408170070c0d084ab76dc99452982501e
giuseppe <giuseppe.sodano.1996@gmail.com>
2026-03-31 09:59:28 +0200

    Update: User.java UserController.java UserRepository.java UserService.java
    Tested:
    UserServiceTest.java

--- src/main/java/com/gestionaleprenotazioni/formerjob/Repository/UserRepository.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.gestionaleprenotazioni.formerjob.Repository;
 
 import com.gestionaleprenotazioni.formerjob.Model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +18,16 @@ public interface UserRepository extends JpaRepository<User, Integer>
 
         @Query(value = "SELECT * FROM User u WHERE u.name = ?1 AND u.surname = ?2", nativeQuery = true)
         User findByNameAndSurname(String name, String surname);
+
+        @Query(value = "SELECT * FROM User u WHERE u.surname =?1 AND u.mail = ?2", nativeQuery = true)
+        User findBySurnameAndEmail(String surname,String email);
+
+        /*
+        @Modifying
+        @Transactional
+        @Query("UPDATE User u SET u.email = :email WHERE u.id = :id")
+        int updateEmail(@Param("id") Integer id, @Param("email") String email);
+        */
+
+
 }
