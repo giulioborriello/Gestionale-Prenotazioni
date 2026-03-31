354195decf834e3e1722f535d6ac778f310147b7
giacopa97 <giacopa97@gmail.com>
2026-03-31 10:39:22 +0200

    Change: EventController.java         EventControllerTest.java         EventRepository.java         EventService.java         EventServiceTest.java

--- src/main/java/com/gestionaleprenotazioni/formerjob/Service/EventService.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Service/EventService.java
@@ -3,8 +3,13 @@ package com.gestionaleprenotazioni.formerjob.Service;
 import com.gestionaleprenotazioni.formerjob.Dto.EventDto;
 import com.gestionaleprenotazioni.formerjob.Mapper.EventMapper;
 import com.gestionaleprenotazioni.formerjob.Model.Event;
+import com.gestionaleprenotazioni.formerjob.Model.Type;
 import com.gestionaleprenotazioni.formerjob.Repository.EventRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService extends AbstractService<Event, EventDto> {
@@ -18,5 +23,40 @@ public class EventService extends AbstractService<Event, EventDto> {
         this.eventRepository = eventRepository;
     }
 
-    // Aggiungere i metodi che utilizzeremo per il nostro applicativo
+    // 🔹 Metodo per trovare l'evento per nome
+    public EventDto findByName(String name) {
+        return eventMapper.toDTO(eventRepository.findByName(name));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public EventDto findByDescription(String description) {
+        return eventMapper.toDTO(eventRepository.findByDescription(description));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public List<EventDto> findByType(Type type) {
+        return eventMapper.toDTOList(eventRepository.findByType(type));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public List<EventDto> findByLocation(String location) {
+        return eventMapper.toDTOList(eventRepository.findByLocation(location));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public List<EventDto> findByDataBetween(Date startDate, Date endDate) {
+        return eventMapper.toDTOList(eventRepository.findByDataBetween(startDate, endDate));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public List<EventDto> findByDataAfter(Date data) {
+        return eventMapper.toDTOList(eventRepository.findByDataAfter(data));
+    }
+
+    // 🔹 Metodo per trovare l'evento per nome
+    public List<EventDto> findByDataBefore(Date data) {
+        return eventMapper.toDTOList(eventRepository.findByDataBefore(data));
+    }
+
+
 }
