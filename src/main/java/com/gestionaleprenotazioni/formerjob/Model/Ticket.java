1606560c1728f27fe41c5387383e702fe462945f
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-03-31 14:56:51 +0200

    fatto piccolo cambiamento a event e ticket per rinominare ticket id

--- src/main/java/com/gestionaleprenotazioni/formerjob/Model/Ticket.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Model/Ticket.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer ticketId;
+    private Integer id;
 
     @Column(nullable = false)
     private String name;
