fd59b4d5487ce865e3e632ac6a7e8954bc0ac486
Daniele Cavarra <danielecavarra97@gmail.com>
2026-04-01 11:34:45 +0200

    modify : placerepository e placeservice

--- src/main/java/com/gestionaleprenotazioni/formerjob/Repository/PlaceRepository.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Repository/PlaceRepository.java
@@ -1,11 +1,20 @@
 package com.gestionaleprenotazioni.formerjob.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.gestionaleprenotazioni.formerjob.Model.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-//rep. accedere ai dati dei posti
+//MODIFIY REPOSITORY DOVREBBE ANDR BENE
+@Repository
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
-	List<Place> findByNome(String nome);
+
+    // posti per nome/codice fratm
+    List<Place> findByCode(String code);
+
+    //posti liberi ooccupati
+    List<Place> findByStatus(boolean status);
+
+    //tutti i posti di un evento
+    List<Place> findByEventId(Integer eventId);
 }
--- src/main/java/com/gestionaleprenotazioni/formerjob/Service/PlaceService.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Service/PlaceService.java
@@ -5,27 +5,34 @@ import com.gestionaleprenotazioni.formerjob.Mapper.PlaceMapper;
 import com.gestionaleprenotazioni.formerjob.Model.Place;
 import com.gestionaleprenotazioni.formerjob.Repository.PlaceRepository;
 import org.springframework.stereotype.Service;
-
+//ultima modifica service dovrebbe andar bene
 import java.util.List;
 
-//service per gestire la logica dei posti
 @Service
 public class PlaceService extends AbstractService<Place, PlaceDto> {
 
-    //repository per accesso ai dati
     private final PlaceRepository placeRepository;
-
-    //mapper per conversione dto - entity
     private final PlaceMapper placeMapper;
 
-    //costruttore
+    //costruttor
     public PlaceService(PlaceRepository placeRepository, PlaceMapper placeMapper) {
         super(placeRepository, placeMapper);
         this.placeRepository = placeRepository;
         this.placeMapper = placeMapper;
     }
 
-    public List<PlaceDto> findByNome(String nome) {
-        return placeMapper.toDTOList(placeRepository.findByNome(nome));
+    //per codice nome
+    public List<PlaceDto> findByCode(String code) {
+        return placeMapper.toDTOList(placeRepository.findByCode(code));
+    }
+
+    //post liberi o occupat
+    public List<PlaceDto> findByStatus(boolean status) {
+        return placeMapper.toDTOList(placeRepository.findByStatus(status));
+    }
+
+    //search dei posti di un certo evento
+    public List<PlaceDto> findByEventId(Integer eventId) {
+        return placeMapper.toDTOList(placeRepository.findByEventId(eventId));
     }
 }
