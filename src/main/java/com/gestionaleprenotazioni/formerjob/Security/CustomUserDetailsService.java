c1a0317cd76030f27c6a0a13abdc14d6d9312c7d
Cap1209 <capozzolilorenzo@outlook.it>
2026-04-01 12:18:51 +0200

    change Lorenzo Capozoli: Task1 - Creazione Profili     - application-dev.properties -> profilo sviluppo     -application-prod.properties -> profilo produzione     -aggiunto spring.profile.active=dev in applicatio.properties -> definisce quale profilo è attivo
      Task2 - Creare la sicurezza e loccarla su un unico profilo
        - Aggiornata classe user con aggiunta di Role (admin e user)
        -SecurityConfig.java -> per gestire regole di accesso (chi può fare GET,POST,PUT e DELETE)
        -CustomUserDetailService.java -> carica l'utente dal DB tramite email.
    
     Ho loccato la security sul profilo prod aggiungendo @Profile("prod") su entrambe le classi, questo significa che la security è attiva solo quando spring.profiles.active=prod.
    
     Aggiornato pom.xml aggiungendo spring-boot-starter-security e spring-security-test senza queste dipendenze la security non funzionerebbe.

---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.gestionaleprenotazioni.formerjob.Security;
+
+import com.gestionaleprenotazioni.formerjob.Model.User;
+import com.gestionaleprenotazioni.formerjob.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Profile("prod") //attivo solo in produzione
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        // cerchiamo l'utente nel DB tramite email
+        User user = userRepository.findByEmail(email);
+
+            if(user == null)
+            {
+                throw new UsernameNotFoundException("Utente non trovato: " +email);
+            }
+
+        // convertiamo il nostro Role in un GrantedAuthority che Spring Security capisce
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+
+        // restituiamo un UserDetails con email, password e ruolo
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(authority)
+        );
+    }
+}
