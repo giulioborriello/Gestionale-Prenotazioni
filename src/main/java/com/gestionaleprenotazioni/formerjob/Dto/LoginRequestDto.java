905aa3beba59e2ebb2448e83f2538dec1ac19d12
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-01 12:58:22 +0200

    implementato login e aggiunto bypass della sicurezza per profilo dev

---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Dto/LoginRequestDto.java
@@ -0,0 +1,17 @@
+package com.gestionaleprenotazioni.formerjob.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    private String email;
+    private String password;
+}
+
---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Security/DevSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.gestionaleprenotazioni.formerjob.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+public class DevSecurityConfig {
+    @Bean
+    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
+
