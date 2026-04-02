20c1a9d9b3b345b60c5499de970d445718dd85a6
gotuax <giuseppe.sodano.1996@gmail.com>
2026-04-02 15:15:53 +0200

    Update: EmailService.java mvnw PdfService.java pom.xml

---
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Service/PdfService.java
@@ -0,0 +1,42 @@
+package com.gestionaleprenotazioni.formerjob.Service;
+
+import org.openpdf.text.Document;
+import org.openpdf.text.Paragraph;
+import org.openpdf.text.pdf.PdfPTable;
+import org.openpdf.text.pdf.PdfWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+
+@Service
+public class PdfService {
+
+    public byte[] generaPdfOrdine(Long ordineId, String cliente, double totale) {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            Document document = new Document();
+            PdfWriter.getInstance(document, out);
+
+            document.open();
+            document.add(new Paragraph("Riepilogo ordine #" + ordineId));
+            document.add(new Paragraph("Cliente: " + cliente));
+            document.add(new Paragraph(" "));
+
+            PdfPTable table = new PdfPTable(2);
+            table.addCell("Campo");
+            table.addCell("Valore");
+            table.addCell("Ordine");
+            table.addCell(String.valueOf(ordineId));
+            table.addCell("Cliente");
+            table.addCell(cliente);
+            table.addCell("Totale");
+            table.addCell(String.valueOf(totale));
+
+            document.add(table);
+            document.close();
+
+            return out.toByteArray();
+        } catch (Exception e) {
+            throw new IllegalStateException("Errore generazione PDF", e);
+        }
+    }
+}
