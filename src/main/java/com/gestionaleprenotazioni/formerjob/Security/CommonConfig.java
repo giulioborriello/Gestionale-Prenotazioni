b5ad79c3fbb45027751eda8366d1c75fcfe6d8a7
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-22 10:12:55 +0200

    Risolto problema di duplicazione del metodo annotato come bean passwordEncoder, nelle classi SecurityConfig.java e CommonConfig.java nel profilo dev

--- src/main/java/com/gestionaleprenotazioni/formerjob/Security/CommonConfig.java
+++ src/main/java/com/gestionaleprenotazioni/formerjob/Security/CommonConfig.java
@@ -2,8 +2,6 @@ package com.gestionaleprenotazioni.formerjob.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,10 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CommonConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
